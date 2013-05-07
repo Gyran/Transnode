e6dfdf45e35a1b391637201306cbb7dee876af7f
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2013-05-07 12:46:44 +0200

    new place for columns

---
+++ public/javascripts/init.js
@@ -0,0 +1,86 @@
+/*
+      App.TorrentColumn.create(),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      }),
+      App.TorrentColumn.create({
+          
+      })
+    ]
+    */
+var settings = {
+    applicationNeeds: Ember.A(),
+    plugins: Ember.A(),
+    torrentColumns: Ember.A(),
+
+    leftColumnViews: Ember.A(),
+    detailsViews: Ember.A(),
+    toolbarViews: Ember.A(),
+
+    addPlugin: function (plugin) {
+        this.plugins.pushObject(plugin);
+    },
+
+    addNeed: function (controller) {
+        this.applicationNeeds.pushObject(controller);
+    },
+
+    addTorrentColumn: function (column) {
+        this.torrentColumns.pushObject(column);
+    },
+
+    getPlugins: function () {
+        return this.plugins;
+    },
+
+    getNeedsArray: function () {
+        return this.applicationNeeds.toArray();
+    },
+
+    getTorrentColumnsArray: function () {
+        return this.torrentColumns.toArray();
+    }
+};
+
+// initial settins
+settings.addNeed('torrents');
+settings.addNeed('selectedTorrents');
+settings.addNeed('torrentColumns');
+
+settings.addTorrentColumn({ name: 'Status',     dataField: 'statusString' });
+settings.addTorrentColumn({ name: 'Name',       dataField: 'name' });
+settings.addTorrentColumn({ name: 'Size',       dataField: 'sizeConverted' });
+settings.addTorrentColumn({ name: 'Done',       dataField: 'percentDoneConverted' });
+settings.addTorrentColumn({ name: 'DL',         dataField: 'downloadedConverted' });
+settings.addTorrentColumn({ name: 'UL',         dataField: 'uploadedConverted' });
+settings.addTorrentColumn({ name: 'Ratio',      dataField: 'ratioConverted' });
+settings.addTorrentColumn({ name: 'Date added', dataField: 'addedDateConverted' });
+settings.addTorrentColumn({ name: 'DL Rate',    dataField: 'rateDownloadConverted' });
+settings.addTorrentColumn({ name: 'UL Rate',    dataField: 'rateUploadConverted' });
+settings.addTorrentColumn({ name: 'ETA',        dataField: 'etaConverted' });
+
+
+var App = Ember.Application.create({
+    LOG_TRANSITIONS: true
+});
--- public/javascripts/plugins.js
+++ public/javascripts/plugins.js
@@ -1,41 +1,11 @@
-
-var settings = {
-    applicationNeeds: Ember.A(),
-    plugins: Ember.A(),
-
-    leftColumnViews: Ember.A(),
-    detailsViews: Ember.A(),
-    toolbarViews: Ember.A(),
-
-    addPlugin: function (plugin) {
-        this.plugins.pushObject(plugin);
-    },
-
-    addNeed: function (controller) {
-        this.applicationNeeds.pushObject(controller);
-    },
-
-    getPlugins: function () {
-        return this.plugins;
-    },
-
-    getNeedsArray: function () {
-        console.log('return needs', this.applicationNeeds.toArray());
-        return this.applicationNeeds.toArray();
-    }
-};
-
-/* init */
-settings.addNeed('torrents');
-settings.addNeed('selectedTorrents');
-settings.addNeed('torrentColumns');
-
 /*** plugins hopefully ***/
 var filtersPlugin = {
     name: 'filters',
 
     init: function () {
-        console.log('init filers!');
+        App.filtersView = Ember.View.extend({
+
+        });
     }
 };
 settings.plugins.pushObject(filtersPlugin);
@@ -45,6 +15,3 @@ settings.plugins.forEach(function(plugin, index, e) {
     plugin.init();
 });
 
-var App = Ember.Application.create({
-    LOG_TRANSITIONS: true
-});
